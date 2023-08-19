ed77c5afe3a917c4e16671d371a79ffdb2557cea
@farzan.siddiqui <farzan.siddiqui@hexnbit.com>
2023-08-20 03:11:27 +0530

    detect plane surface.

--- app/src/main/java/com/siddiqui/androidar/MainActivity.kt
+++ app/src/main/java/com/siddiqui/androidar/MainActivity.kt
@@ -1,28 +1,58 @@
 package com.siddiqui.androidar
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import com.google.android.material.snackbar.Snackbar
 import com.google.ar.core.ArCoreApk
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
 import com.siddiqui.androidar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    var yourModelRenderable: ModelRenderable? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         maybeEnableArButton()
+
+        val arFragment = supportFragmentManager.findFragmentById(R.id.ar_fragment) as ArFragment
+
+        arFragment.setOnTapArPlaneListener { hitResult, _, _ ->
+            val anchor = hitResult.createAnchor()
+            val anchorNode = AnchorNode(anchor)
+            anchorNode.setParent(arFragment.arSceneView.scene)
+            val modelNode = TransformableNode(arFragment.transformationSystem)
+            modelNode.setParent(anchorNode)
+            modelNode.renderable = yourModelRenderable
+            modelNode.select()
+        }
+
+        ModelRenderable.builder()
+            .setSource(this, Uri.parse("models/cube.glb")) // or "model.glb"
+            .build()
+            .thenAccept { renderable ->
+                yourModelRenderable = renderable
+            }
+            .exceptionally { throwable ->
+                Log.e("ARApp", "Error loading model", throwable)
+                null
+            }
+
     }
 
     private fun maybeEnableArButton() {
         ArCoreApk.getInstance().checkAvailabilityAsync(this) { availability ->
             if (availability.isSupported) {
-                binding.mArButton.visibility = View.VISIBLE
-                binding.mArButton.isEnabled = true
+                    showSnackbar(binding.mainLayout,"ARCore Support Device ")
             } else { // The device is unsupported or unknown.
-                binding.mArButton.visibility = View.INVISIBLE
-                binding.mArButton.isEnabled = false
+                    showSnackbar(binding.mainLayout,"ARCore doesn't support this device")
             }
         }
     }
@@ -30,5 +60,8 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
 
+    }
+    private fun showSnackbar(view: View, message: String, duration: Int = Snackbar.LENGTH_SHORT) {
+        Snackbar.make(view, message, duration).show()
     }
 }
