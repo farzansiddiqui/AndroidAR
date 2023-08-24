33fb5fc80c5a937d06d0bc2066043faf36f896ce
farzan.siddiqui <farzan.siddiqui@hexnbit.com>
2023-08-24 18:51:49 +0530

    module added. build application getting error

--- app/src/main/java/com/siddiqui/androidar/MainActivity.kt
+++ app/src/main/java/com/siddiqui/androidar/MainActivity.kt
@@ -1,31 +1,82 @@
 package com.siddiqui.androidar
 
+import android.app.ActivityManager
+import android.content.Context
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import com.google.ar.core.ArCoreApk
+import android.view.MotionEvent
+import android.widget.Toast
+import com.google.ar.core.Anchor
+import com.google.ar.core.HitResult
+import com.google.ar.core.Plane
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.rendering.Renderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
 import com.siddiqui.androidar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val MIN_OPENGL_VERSION = 3.0
+    private lateinit var arFragment: ArFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        maybeEnableArButton()
-    }
 
-    private fun maybeEnableArButton() {
-        ArCoreApk.getInstance().checkAvailabilityAsync(this) { availability ->
-            if (availability.isSupported) {
-                binding.mArButton.visibility = View.VISIBLE
-                binding.mArButton.isEnabled = true
-            } else { // The device is unsupported or unknown.
-                binding.mArButton.visibility = View.INVISIBLE
-                binding.mArButton.isEnabled = false
-            }
+        isDeviceArSupported(this)
+
+        arFragment =
+            (supportFragmentManager.findFragmentById(R.id.sceneform_ar_scene_view) as ArFragment?)!!
+        this.arFragment.setOnTapArPlaneListener { hitResult: HitResult, _: Plane?, motionEvent: MotionEvent? ->
+            val anchor = hitResult.createAnchor()
+            placeObjectOnScene(arFragment, anchor, Uri.parse("model.glb"))
         }
     }
+
+
+    private fun isDeviceArSupported(context : Context) : Boolean {
+        val openGlVersionString = (context.getSystemService(ACTIVITY_SERVICE) as ActivityManager)
+            .deviceConfigurationInfo
+            .glEsVersion
+        if (openGlVersionString.toDouble() < MIN_OPENGL_VERSION) {
+
+            Toast.makeText(this, "Minimum Open GL version should be 3 or later", Toast.LENGTH_LONG)
+                .show()
+            this.finish()
+            return false
+        }
+        return true
+    }
+    private fun addModelToScene(arFragment: ArFragment, anchor: Anchor, renderable: Renderable) {
+        val transformableNode = TransformableNode(arFragment.transformationSystem)
+        transformableNode.renderable = renderable
+
+        val anchorNode = AnchorNode(anchor)
+        transformableNode.setParent(anchorNode)
+        arFragment.arSceneView.scene.addChild(anchorNode)
+        transformableNode.select()
+    }
+    private fun placeObjectOnScene(fragment: ArFragment, anchor: Anchor, uri: Uri) {
+        ModelRenderable.builder()
+            .setSource(fragment.context, uri)
+            .build()
+            .thenAccept { renderable: ModelRenderable? ->
+                addModelToScene(
+                    fragment, anchor, renderable!!
+                )
+            }
+            .exceptionally { throwable: Throwable ->
+                Toast.makeText(
+                    fragment.context, "Error:" + throwable.message,
+                    Toast.LENGTH_LONG
+                ).show()
+                null
+            }
+    }
+
 
     override fun onResume() {
         super.onResume()
