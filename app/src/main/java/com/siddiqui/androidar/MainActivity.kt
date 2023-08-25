62af0338f7dbe31c2c1adc1f607c6045a3cf62b4
farzan.siddiqui <farzan.siddiqui@hexnbit.com>
2023-08-25 18:46:43 +0530

    upload file

--- app/src/main/java/com/siddiqui/androidar/MainActivity.kt
+++ app/src/main/java/com/siddiqui/androidar/MainActivity.kt
@@ -2,25 +2,18 @@ package com.siddiqui.androidar
 
 import android.app.ActivityManager
 import android.content.Context
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MotionEvent
 import android.widget.Toast
-import com.google.ar.core.Anchor
-import com.google.ar.core.HitResult
-import com.google.ar.core.Plane
-import com.google.ar.sceneform.AnchorNode
-import com.google.ar.sceneform.rendering.ModelRenderable
-import com.google.ar.sceneform.rendering.Renderable
-import com.google.ar.sceneform.ux.ArFragment
-import com.google.ar.sceneform.ux.TransformableNode
 import com.siddiqui.androidar.databinding.ActivityMainBinding
+import io.github.sceneview.ar.node.ArModelNode
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val MIN_OPENGL_VERSION = 3.0
-    private lateinit var arFragment: ArFragment
+
+    lateinit var modelNode: ArModelNode
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -28,15 +21,13 @@ class MainActivity : AppCompatActivity() {
 
         isDeviceArSupported(this)
 
-        arFragment =
-            (supportFragmentManager.findFragmentById(R.id.sceneform_ar_scene_view) as ArFragment?)!!
-        this.arFragment.setOnTapArPlaneListener { hitResult: HitResult, _: Plane?, _: MotionEvent? ->
-            val anchor = hitResult.createAnchor()
-            placeObjectOnScene(arFragment, anchor, Uri.parse("chair.sfb"))
-        }
+        modelNode = ArModelNode().
+
+
+
+
+
     }
-
-
     private fun isDeviceArSupported(context : Context) : Boolean {
         val openGlVersionString = (context.getSystemService(ACTIVITY_SERVICE) as ActivityManager)
             .deviceConfigurationInfo
@@ -44,37 +35,12 @@ class MainActivity : AppCompatActivity() {
         if (openGlVersionString.toDouble() < MIN_OPENGL_VERSION) {
 
             Toast.makeText(this, "Minimum Open GL version should be 3 or later", Toast.LENGTH_LONG)
+
                 .show()
             this.finish()
             return false
         }
         return true
-    }
-    private fun addModelToScene(arFragment: ArFragment, anchor: Anchor, renderable: Renderable) {
-        val transformableNode = TransformableNode(arFragment.transformationSystem)
-        transformableNode.renderable = renderable
-
-        val anchorNode = AnchorNode(anchor)
-        transformableNode.setParent(anchorNode)
-        arFragment.arSceneView.scene.addChild(anchorNode)
-        transformableNode.select()
-    }
-    private fun placeObjectOnScene(fragment: ArFragment, anchor: Anchor, uri: Uri) {
-        ModelRenderable.builder()
-            .setSource(fragment.context, uri)
-            .build()
-            .thenAccept { renderable: ModelRenderable? ->
-                addModelToScene(
-                    fragment, anchor, renderable!!
-                )
-            }
-            .exceptionally { throwable: Throwable ->
-                Toast.makeText(
-                    fragment.context, "Error:" + throwable.message,
-                    Toast.LENGTH_LONG
-                ).show()
-                null
-            }
     }
 
 
